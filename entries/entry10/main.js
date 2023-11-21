1b78fe36a7a564a40972c41039ff64d772fe0afe
Christieywwong <wongc226@newschool.edu>
2023-11-21 18:49:01 -0500

    entry10

--- entries/entry10/main.js
+++ entries/entry10/main.js
@@ -1,4 +1,4 @@
-console.log("hello!");
+// console.log("hello!");
 
 // function turnOnLight() {
 //     console.log("Lights on!");
@@ -19,12 +19,25 @@ console.log("hello!");
 // button.addEventListener("click", turnOnLight);
 
 
+// let body = document.body;
+
+// function turnOnLight() {
+//   body.classList.toggle("light");
+//   button.classList.toggle("buttonOn");
+// }
+
+// let button = document.querySelector(".button");
+// button.addEventListener("click", turnOnLight);
+
+
 let body = document.body;
 
-function turnOnLight() {
-  body.classList.toggle("light");
-  button.classList.toggle("buttonOn");
+
+function Change() {
+  document.getElementById("ChangeText").innerHTML = "My dad has a rather simple taste. He prefers and pretty much only has one type of clothing in his closet, which consists of the black and white outfits he wears to work. However, he is a big denim fan for some reason and owns many denim items, including denim jeans, shorts, and jackets in various colors. During weekends, he wears them on different occasions. In particular, he loves to wear one of his denim jackets that has a strong smell attached to it. So now, whenever I wear my denim jacket, I immediately think of my dad.";
+  document.getElementById("ChangeTitle").innerHTML = "THE STORY OF MY DAD AND HIS DENIM JACKET";
 }
 
+
 let button = document.querySelector(".button");
-button.addEventListener("click", turnOnLight);
+button.addEventListener("click", Change);
